666739bec6ad279f7c9221f12bf3c046fd4af207
Krishnendu Mukherjee <krishnendu.vawsum@gmail.com>
2018-06-27 17:21:27 +0530

    Complete project has been added

---
+++ app/src/main/java/com/example/workspace/adapter/implementations/Krishnendu.java
@@ -0,0 +1,16 @@
+package com.example.workspace.adapter.implementations;
+
+import com.example.workspace.adapter.interfaces.Bengali;
+
+/**
+ * Created by Workspace on 6/27/2018.
+ */
+
+public class Krishnendu implements Bengali
+{
+    @Override
+    public String speak ()
+    {
+        return "The person speaks in Bengali";
+    }
+}
